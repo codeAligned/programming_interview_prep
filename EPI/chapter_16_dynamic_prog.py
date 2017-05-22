07dea1daea5c3da49e27e5118bdb4e96d813f425
alexhchao <alexchao86@gmail.com>
2017-05-21 20:20:27 -0400

    new chapter - dynamic programming

---
+++ EPI/chapter_16_dynamic_prog.py
@@ -0,0 +1,8 @@
+# ************************************************************************************
+# INTERVIEW PREP - Programming Interview Questions from
+# Elements of Programming Interviews
+# Created 5-21-2017
+# Author: Alex H Chao
+# ************************************************************************************
+
+
