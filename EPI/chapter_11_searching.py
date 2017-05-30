db31eddb89deb6eb040c9a72f3ec98fd1d41aa02
Alex <alexchao86@gmail.com>
2017-05-29 22:33:27 -0400

    added 11.8 - partition 2

--- EPI/chapter_11_searching.py
+++ EPI/chapter_11_searching.py
@@ -8,7 +8,7 @@
 import math as m
 import numpy as np
 from helper.helper import *
-
+from random import randint
 # ************************************************************************************
 # 11.8 - Find the k-th largest element (Quick select)
 # assume array is unsorted and all entries are distinct
@@ -18,7 +18,7 @@ from helper.helper import *
 
 
 # need to also pass in left, right, pivot_index
-def partition(a):
+def partition(a, lo, hi):
     """
     
     Parameters
@@ -32,15 +32,15 @@ def partition(a):
 
     #set partition to be last element
     p = len(a)-1
-    hi = p-1
-    lo = 0
+    hi = p-1 if p is None else hi
+    lo = 0 if lo is None else lo
 
     while lo < hi:
-        if (a[lo] > p) & (a[hi] < p): #we need to swap
+        if (a[lo] > a[p]) & (a[hi] < a[p]): #we need to swap
             swap(a,lo,hi)
-        elif a[lo] > p: # only lo is greater than p
+        elif a[lo] > a[p]: # only lo is greater than p
             hi -= 1
-        elif a[hi] < p:
+        elif a[hi] < a[p]:
             lo += 1
         #elif (a[lo] < p) & (a[hi] > p): #we good
         else:
@@ -48,11 +48,33 @@ def partition(a):
             hi -= 1
 
     #swap out the pivot
-    swap(a,hi,p)
+    new_p = hi if a[hi] > a[p] else hi+1
+
+    swap(a,new_p,p)
+    return new_p
 
 
+a = [randint(1,9) for x in range (10)]
 
-a = [9,9,9,9,1,5]
+
+k=5
+
+# STILL IN PROGRESS
+
+def get_kth_largest_element(a,lo,hi, k):
+
+    p = partition(a,lo,hi)
+    # kth largest = len(a) - p
+    k_th_largest = len(a) - k
+    if p == k_th_largest: # were done
+        return a[p]
+    elif p > k_th_largest: #
+        get_kth_largest_element(a, 0, p, k)
+    else: # p < k
+        get_kth_largest_element(a, p+1, len(a)-1, k)
+
+k=5
+get_kth_largest_element(a,0,len(a)-1, 5)
 
 partition(a)
 print(a)
