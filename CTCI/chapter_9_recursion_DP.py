565835b52abe1c523e3326ed16a938d20b476a87
Alex <alexchao86@gmail.com>
2017-06-19 21:12:02 -0400

    added CTCI - 9.2 Ways to traverse 2D array

--- CTCI/chapter_9_recursion_DP.py
+++ CTCI/chapter_9_recursion_DP.py
@@ -4,7 +4,7 @@
 # Created 6-18-2017
 # Author: Alex H Chao
 # ************************************************************************************
-import math as m
+import math
 import numpy as np
 import pandas as pd
 from helper.helper import *
@@ -52,6 +52,68 @@ def test_staircase():
 # ************************************************************************************
 # 9.2 2-D Ways to Traverse - Robot problem - how many paths to go from (0,0) to (X,Y) ?
 # ************************************************************************************
+# base case is we hit a corner (e.g. x <= 0 or y <= 0) return 1
+# (0,0) -> 0
+# (x, y) = (X-1, Y) + (X, Y-1)
+
+# Runtime: niave algo -> O(2 ^ X+Y) vs DP algo = O(X*Y)
+
+@timeit
+def traverse_2d_array(m,x,y):
+    if x == y == 0:
+        return 0
+    elif x <= 0 or y <= 0:
+        return 1
+    else:
+        m[x,y] = traverse_2d_array(m,x-1,y) + traverse_2d_array(m, x,y-1)
+
+
+# no recursion method
+
+def count_ways(m):
+    num_rows = m.shape[0]
+    num_cols = m.shape[1]
+    m[0,:] = 1
+    m[:,0] = 1
+
+    for row in range(1,num_rows):
+        for col in range(1,num_cols):
+            m[row,col] = m[row-1,col] + m[row, col-1]
+    return m
+
+# this way (leetcode) doesnt involve numpy
+
+def initialize_matrix_zeros(rows,cols):
+    return [[0 for x in range(cols)] for y in range(rows)]
+
+# working, submitted on leetcode
+
+def count_ways(n,m):
+    A = np.zeros([n,m])
+    A[0, :] = 1
+    A[:, 0] = 1
+
+    for row in range(1, n):
+        for col in range(1, m):
+            A[row, col] = A[row - 1, col] + A[row, col - 1]
+    return int(A[row,col])
+
+def count_ways_2D_array(x,y):
+    return math.factorial(x+y) / (math.factorial(x) * math.factorial(y))
+
+n = 5
+m = np.zeros([n, n])
+
+n=3
+m=4
+count_ways(4,4)
+
+count_ways_2D_array(3,3)
+
+#random: u can do tuples in list comprehensions too
+
+[(x, x+1) for x in range(4)]
+
 
 # ************************************************************************************
 # 9.3 Magic or Equilibrium Index of Array is such that A[i] = i
